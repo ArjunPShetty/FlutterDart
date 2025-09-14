15c58c310d8688eb5eca1d0310209b97ac4089ca
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2025-09-14 15:21:09 +0530

    Add files via upload

---
+++ Expanded_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Row(
+          children:[
+            Expanded(
+
+              flex: 2,// flex means ratio in a screen which it divide by its requirement
+              child: Container(
+                width: 50,
+                height: 100,
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+
+              flex: 3,
+              child: Container(
+                width: 50,
+                height: 100,
+                color: Colors.cyanAccent,
+              ),
+            ),
+            Expanded(
+
+              flex: 4,
+              child: Container(
+                width: 50,
+                height: 100,
+                color: Colors.amber,
+              ),
+            ),
+          ]
+      ),
+    );
+  }
+}
---
+++ rounded_btn_widgets.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String btnName;
+  final Icon?icon;
+  final Color?bgcolour;
+  final TextStyle?textStyle;
+  final VoidCallback?callback;
+
+  RoundedButton(
+  { required this.btnName,
+    this.icon,
+    this.bgcolour=Colors.blue,
+    this.textStyle,
+    this.callback,}
+      );
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: (){
+      callback!();
+    }, child: icon!= null?
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon!,
+            Container(
+              width: 11,
+            ),
+            Text(btnName,
+            style: textStyle,)
+          ],
+        )
+       : Text(btnName,
+            style: textStyle),
+      style: ElevatedButton.styleFrom(
+        shadowColor:bgcolour,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(21)
+          )
+        )
+      ),
+    );
+  }
+}
